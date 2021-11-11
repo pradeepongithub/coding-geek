e5136179444f89e88be03ffe9071cb88cf872528
Pradeep Yadav <pradeep.kumar@maersk.com>
2021-11-11 19:26:55 +0530

    count all anagrams sliding window

--- src/FixSlidingWindow.java
+++ src/FixSlidingWindow.java
@@ -1,6 +1,6 @@
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.List;
+import java.util.Map;
 import java.util.Queue;
 
 import static java.lang.System.*;
@@ -9,7 +9,7 @@ public class FixSlidingWindow {
 
     public static void main(String[] args) {
 
-        //problem: Maximum Sum Subarray of size K
+        //problem: Maximum Sum Sub array of size K
 
         int[] arr = {4, 10, 2, 18, 3, 12, 2, 6, 7, 1, 4, 8};
         int k = 4;
@@ -28,12 +28,13 @@ public class FixSlidingWindow {
             }
         }
 
-        //System.out.println(max);
-
         int[] arr1 = {7, 9, -1, 2, 3, 4, -2, -3, -4};
 
         //getFirstNegativeNoInWindowSizeK(3, arr1);
-        firstNegativeNumberSlidingWindow(3, arr1);
+        //firstNegativeNumberSlidingWindow(3, arr1);
+        //countNumberOfAnagramBruteForce("abc", "abccbabcazb");
+        countAllAnagramSlidingWindow("abc", "abcvbacbacab");
+
 
     }
 
@@ -76,6 +77,85 @@ public class FixSlidingWindow {
                 j++;
             }
         }
+
+    }
+
+    public static int countNumberOfAnagramBruteForce(String p, String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        char[] chars = p.toCharArray();
+        char[] sChars = s.toCharArray();
+        int count = 0;
+        int anagramCount = 0;
+        int k = p.length();
+
+        for (int i = 0; i < k; i++) {
+            if (map.containsKey(chars[i])) {
+                int n = map.get(chars[i]);
+                map.put(chars[i], ++n);
+            } else {
+                map.put(chars[i], 1);
+            }
+        }
+
+        for (int i = 0; i < (s.length() - k); i++) {
+            if (map.size() == count) {
+                anagramCount++;
+            }
+            count = 0;
+            for (int j = i; j < i + k; j++) {
+                if (map.containsKey(sChars[j])) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+
+        }
+        return anagramCount;
+
+    }
+
+    public static void countAllAnagramSlidingWindow(String p, String s) {
+
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < p.length(); i++) {
+            char c = p.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        int i = 0, j = 0, count = map.size(), ans = 0, k = p.length(), n = s.length();
+
+        while (j < n) {
+            char c = s.charAt(j);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) - 1);
+            }
+            if (map.containsKey(c) && map.get(c) == 0) {
+                count--;
+            }
+            if (j - i + 1 < k) {
+                j++;
+            } else if (j - i + 1 == k) {
+
+                if (count == 0) {
+                    ans++;
+                }
+                c = s.charAt(i);
+                if (map.containsKey(c)) {
+                    map.put(c, map.get(c) + 1);
+                }
+                if (map.containsKey(c) && map.get(c) > 0) {
+                    count++;
+                }
+                i++;
+                j++;
+
+            }
+        }
+        out.println(ans);
 
     }
 
